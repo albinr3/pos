56afe25461736530d2c5fe27e843378ae8a886eb
Albin <albinmrodriguez@gmail.com>
2026-03-23 16:49:52 -0400

    sdsds

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -86,9 +86,9 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
+          "max-h-80 overflow-y-auto p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "w-full min-w-[var(--radix-select-trigger-width)]"
         )}
       >
         {children}
